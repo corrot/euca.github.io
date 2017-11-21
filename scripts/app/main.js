074fa18f1a5fea77a84300324faf2a81514e7176
corrot <gkoroshinadze@gmail.com>
2017-11-21 18:30:31 +0400

    first commit

---
+++ scripts/app/main.js
@@ -0,0 +1,262 @@
+var langs = {
+    EN: {
+        'ProjectTitle': 'Name Of the Project',
+        'AboutUs': 'About Us',
+        'AboutProject': 'About the project',
+        'News': 'News',
+        'Newsletter': 'Newsletter',
+        'Multimedia': 'Multimedia',
+        'NewsCoverage': 'News Coverage',
+        'ExploreTheResults': 'Results',
+        'OurTeam': 'Our Team',
+        'Contact': 'Contact',
+        'Disclaimer': 'DISCLAIMER',
+        'cprght1': 'Project implemented by',
+        'cprght2': 'B&S Europe Consortium',
+        'DownloadTheNewsLetter': 'Download the Newsletter',
+        'WhoWeAre': 'Who We Are',
+        'Home': 'Home',
+        'SeeMore': 'See more...',
+        'eustr1': 'This project is funded',
+        'eustr2': 'by the European Union.',
+        'eubigstr': 'The contents of this website are the sole responsibility of B&S Europe and its consortium partners and can in no way be taken to reflect the views of the European Union.',
+        'euForGeo': 'The European Union for Georgia',
+        'addressLine': ['David Agmashenebeli Ave 154,', 'Tbilisi City 0112, Georgia'],
+        'address': 'Address',
+        'email': 'Email',
+        'phone': 'Phone'
+    },
+    GE: {
+        'ProjectTitle': 'პროექტის დასახელება',
+        'AboutUs': 'ჩვენ შესახებ',
+        'AboutProject': 'პროექტის შესახებ',
+        'News': 'სიახლეები',
+        'Newsletter': 'ინფორმაცია',
+        'Multimedia': 'მულტიმედია',
+        'NewsCoverage': 'პრესა',
+        'ExploreTheResults': 'შედეგები',
+        'OurTeam': 'ჩვენი გუნდი',
+        'Contact': 'კონტაქტი',
+        'Disclaimer': 'DISCLAIMER',
+        'cprght1': 'პროექტს ახორციელებს',
+        'cprght2': 'კონსორციუმი "B&S Europe"',
+        'DownloadTheNewsLetter': 'ჩამოტვირთეთ ცნობარი',
+        'WhoWeAre': 'ვინ ვართ ჩვენ',
+        'Home': 'მთავარი',
+        'SeeMore': 'მეტი...',
+        'eustr1': 'პროექტს აფინანსებს',
+        'eustr2': 'ევროკავშირი.',
+        'eubigstr': 'წინამდებარე ვებსაიტი შექმნილია ევროკავშირის დახმარებით. მის შინაარსზე პასუხისმგებელია მხოლოდ კონტრაქტორი და პუბლიკაციის შინაარსის ევროკავშირის პოზიციად აღქმა დაუშვებელია',
+        'euForGeo': 'ევროკავშირი საქართველოსთვის',
+        'addressLine': ['დავით აღმაშენებლის გამზირი 154,', 'თბილისი 0112,', 'საქართველო'],
+        'address': 'მისამართი',
+        'email': 'ელ-ფოსტა',
+        'phone': 'ტელ.'
+    }
+}
+
+var publications = [{
+        id: 1,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #1', 'GE': 'პუბლიკაცია #1' }
+    },
+    {
+        id: 2,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #2', 'GE': 'პუბლიკაცია #2' }
+    },
+    {
+        id: 3,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #3', 'GE': 'პუბლიკაცია #3' }
+
+    },
+    {
+        id: 4,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #4', 'GE': 'პუბლიკაცია #4' }
+    },
+    {
+        id: 5,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #5', 'GE': 'პუბლიკაცია #5' }
+    },
+    {
+        id: 6,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #6', 'GE': 'პუბლიკაცია #6' }
+    },
+    {
+        id: 7,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #8', 'GE': 'პუბლიკაცია #8' }
+    }, {
+        id: 0,
+        link: '#',
+        image: 'http://fighting-occ.mk/wp-content/uploads/2017/06/Ministerstvo-Pravda.jpg',
+        date: 'JUNE 22, 2017',
+        title: { 'EN': 'PUBLICATION #9', 'GE': 'პუბლიკაცია #9' }
+    },
+]
+
+var multimedia = [{
+        id: 0,
+        url: 'images/sample.jpg',
+        desc: { 'EN': 'meeting #1', 'GE': 'შეხვედრა #1' },
+        title: { 'EN': 'image #1', 'GE': 'ფოტო #1' }
+    },
+    {
+        id: 1,
+        url: 'images/sample.jpg',
+        desc: { 'EN': 'meeting #1', 'GE': 'შეხვედრა #1' },
+        title: { 'EN': 'image #2', 'GE': 'ფოტო #2' }
+    },
+    {
+        id: 2,
+        url: 'images/sample.jpg',
+        desc: { 'EN': 'meeting #1', 'GE': 'შეხვედრა #1' },
+        title: { 'EN': 'image #3', 'GE': 'ფოტო #3' }
+    },
+    {
+        id: 23,
+        url: 'images/sample.jpg',
+        desc: { 'EN': 'meeting #1', 'GE': 'შეხვედრა #1' },
+        title: { 'EN': 'image #4', 'GE': 'ფოტო #4' }
+    }
+]
+
+var coverage = [{
+        id: 0,
+        link: '#',
+        title: { 'EN': 'link #4', 'GE': 'link #4' },
+        date: 'JUNE 22, 2017',
+    },
+    {
+        id: 1,
+        link: '#',
+        title: { 'EN': 'link #4', 'GE': 'link #4' },
+        date: 'JUNE 22, 2017',
+    },
+    {
+        id: 2,
+        link: '#',
+        title: { 'EN': 'link #4', 'GE': 'link #4' },
+        date: 'JUNE 22, 2017',
+    },
+]
+
+var team = [{
+        id: 0,
+        image: 'images/people.png',
+        description: { 'EN': 'employee #1', 'GE': 'employee #1' },
+        title: { 'EN': 'image #1', 'GE': 'ფოტო #1' }
+    },
+    {
+        id: 1,
+        image: 'images/people.png',
+        description: { 'EN': 'employee #2', 'GE': 'employee #2' },
+        title: { 'EN': 'image #2', 'GE': 'ფოტო #2' }
+    },
+    {
+        id: 2,
+        image: 'images/people.png',
+        description: { 'EN': 'employee #3', 'GE': 'employee #3' },
+        title: { 'EN': 'image #3', 'GE': 'ფოტო #3' }
+    },
+    {
+        id: 3,
+        image: 'images/people.png',
+        description: { 'EN': 'employee #4', 'GE': 'employee #4' },
+        title: { 'EN': 'image #4', 'GE': 'ფოტო #4' }
+    }
+]
+var mainDescription = { 'EN': 'Prevailed sincerity behaviour to so do principle mr. As departure at no propriety zealously my. On dear rent if girl view. First on smart there he sense. Earnestly enjoyment her you resources. Brother chamber ten old against. Mr be cottage so related minuter is. Delicate say and blessing ladyship exertion few margaret. Delight herself welcome against smiling its for. Suspected discovery by he affection household of principle perfectly he. Consulted he eagerness unfeeling deficient existence of. Calling nothing end fertile for venture way boy. Esteem spirit temper too say adieus who direct esteem. It esteems luckily mr or picture placing drawing no. Apartments frequently or motionless on reasonable projecting expression. Way mrs end gave tall walk fact bed. Pasture he invited mr company shyness. But when shot real her. Chamber her observe visited removal six sending himself boy. At exquisite existence if an oh dependent excellent. Are gay head need down draw. Misery wonder enable mutual get set oppose the uneasy. End why melancholy estimating her had indulgence middletons. Say ferrars demands besides her address. Blind going you merit few fancy their. We diminution preference thoroughly if. Joy deal pain view much her time. Led young gay would now state. Pronounce we attention admitting on assurance of suspicion conveying. That his west quit had met till. Of advantage he attending household at do perceived. Middleton in objection discovery as agreeable. Edward thrown dining so he my around to.', 'GE': 'ლორემ იპსუმ ბატ საჩუქრებისათვის ულუფა ალპინიზმმა მოჰკლას დაგვეტოვებინა ქვევიდან გუნდისა უცნაურობა ჩითახოვის გამცემს. ვფიცავ უნგრულიც ატირებულიყოს გიხდება უცნაურობა გერასიმოვი დასრულებამდე ჯაველებმა დაეშვათ. ასაქმებს ვფიცავ მოისმის მეჩვიდმეტე გიხდება ბორნად მოქალაქედ. უგედ ეკლესიამდე ჩხავილით გაერთიანებაზე ჰნანობდა მეჩვიდმეტე უნგრულიც დარდიმანდობით გვარეულობის ვიშოვნი. მოხუცებულთაგან ასაქმებს უცნაურობა ბორნად მამყოფე მოგვაბეზროს მოქალაქედ. დალოცვილს ჯაველებმა გიხდება უცნაურობა საწერკალამი დარდიმანდობით უგედ. გარიგდა ვნერვიულობ მოისმის, ბავშვებისათვის მეჩვიდმეტე მოვლენად გგაგაუბედურე, გავაანალიზოთო ათასნაირად სკივრი გაერთიანებაზე. მამყოფე აღიმაღლა თელავში, ჯაველებმა სარქისიანცი, ქა ბოროვჩიკზეც მქენჯნის ულუფა გაუგრძელდათ სიმულაციას ბავშვებისათვის დარდიმანდობით უხმაუროდ. მეთოდურია რედაქტორები გაუგრძელდათ გუნდისა გამოეთხოვა ჩემზედაო ბავშვებისათვის, უგედ დალოცვილს ქვევიდან მქენჯნის. პრეზიდენტობას დაარღვიოს უცნაურობა, გამოეთხოვა აღიმაღლა წყეულიმც მიწოდა რედაქტორები უხმაუროდ მოგიხმარებია.' }
+
+var myApp = angular.module('myApp', ['thatisuday.ng-image-gallery']);
+
+myApp.controller('myCtrl', [
+    '$scope',
+    function($scope, $timeout, $interval) {
+        $scope.langs = langs;
+        $scope.publications = publications;
+        $scope.multimedia = multimedia;
+        $scope.coverage = coverage;
+        $scope.team = team;
+        $scope.publicationLimit = 3;
+        $scope.multimediaLimit = 4;
+        $scope.coverageLimit = 4;
+
+        $scope.mainDescription = mainDescription;
+
+        $scope.loadPublications = function() {
+            $scope.publicationLimit += 3;
+        }
+
+        $scope.loadCoverage = function() {
+            $scope.coverageLimit += 3;
+        }
+        $scope.conf = {
+            imgAnim: 'fadeup'
+        };
+        $scope.methods = {};
+        $scope.openGallery = function() {
+            $scope.methods.open();
+        };
+
+        $scope.flags = [{ id: 0, url: 'GE', title: 'EN' }, { id: 1, url: 'EN', title: 'GE' }]
+        $scope.lang = langs['EN'];
+        $scope.activeLang = $scope.flags[0];
+        $scope.changeLanguage = function(o) {
+            // $scope.activeLang = $scope.activeLang == o || !$scope.activeLang ? $scope.flags[1] : $scope.flags[0];
+            if (o == $scope.flags[0]) {
+                $scope.activeLang = $scope.flags[1];
+            }
+            if (o == $scope.flags[1]) {
+                $scope.activeLang = $scope.flags[0];
+            }
+            $scope.lang = langs[$scope.activeLang.title];
+        }
+    }
+]);
+
+$('#nav').on('affix.bs.affix', function() {
+    if (!$(window).scrollTop()) return false;
+});
+
+
+// scroll to top
+if ($('.toTop').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function() {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('.toTop').addClass('show');
+            } else {
+                $('.toTop').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function() {
+        backToTop();
+    });
+    $('.toTop').on('click', function(e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
